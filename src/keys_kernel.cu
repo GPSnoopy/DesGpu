051a114b2015c3eec74e8dc60f5b370961949cdc
Tanguy Fautre <tanguy@fautre.com>
2021-06-01 23:31:36 +0100

    Simplify macro names

--- src/keys_kernel.cu
+++ src/keys_kernel.cu
@@ -11,13 +11,6 @@
 
 #define ITER_COUNT 1
 
-#define kvtype vtype
-#define kvand vand
-#define kvor vor
-#define kvshl1 vshl1
-#define kvshl vshl
-#define kvshr vshr
-
 #define mask01 0x01010101
 #define mask02 0x02020202
 #define mask04 0x04040404
@@ -27,157 +20,157 @@
 #define mask40 0x40404040
 #define mask80 0x80808080
 
-#define kvand_shl1_or(dst, src, mask)		\
-	kvand(tmp, src, mask); 					\
-	kvshl1(tmp, tmp);						\
-	kvor(dst, dst, tmp)
+#define vand_shl1_or(dst, src, mask)		\
+	vand(tmp, src, mask); 					\
+	vshl1(tmp, tmp);						\
+	vor(dst, dst, tmp)
 
-#define kvand_shl_or(dst, src, mask, shift) \
-	kvand(tmp, src, mask); 					\
-	kvshl(tmp, tmp, shift); 				\
-	kvor(dst, dst, tmp)
+#define vand_shl_or(dst, src, mask, shift)	\
+	vand(tmp, src, mask); 					\
+	vshl(tmp, tmp, shift); 					\
+	vor(dst, dst, tmp)
 
-#define kvand_shl1(dst, src, mask) 			\
-	kvand(tmp, src, mask) ;					\
-	kvshl1(dst, tmp)
+#define vand_shl1(dst, src, mask) 			\
+	vand(tmp, src, mask) ;					\
+	vshl1(dst, tmp)
 
-#define kvand_or(dst, src, mask) 			\
-	kvand(tmp, src, mask); 					\
-	kvor(dst, dst, tmp)
+#define vand_or(dst, src, mask) 			\
+	vand(tmp, src, mask); 					\
+	vor(dst, dst, tmp)
 
-#define kvand_shr_or(dst, src, mask, shift)	\
-	kvand(tmp, src, mask); 					\
-	kvshr(tmp, tmp, shift); 				\
-	kvor(dst, dst, tmp)
+#define vand_shr_or(dst, src, mask, shift)	\
+	vand(tmp, src, mask); 					\
+	vshr(tmp, tmp, shift); 					\
+	vor(dst, dst, tmp)
 
-#define kvand_shr(dst, src, mask, shift) 	\
-	kvand(tmp, src, mask); 					\
-	kvshr(dst, tmp, shift)
+#define vand_shr(dst, src, mask, shift) 	\
+	vand(tmp, src, mask); 					\
+	vshr(dst, tmp, shift)
 
 #define LOAD_V 							\
-	kvtype v0 = *(kvtype*) &vp[0]; 		\
-	kvtype v1 = *(kvtype*) &vp[1]; 		\
-	kvtype v2 = *(kvtype*) &vp[2]; 		\
-	kvtype v3 = *(kvtype*) &vp[3]; 		\
-	kvtype v4 = *(kvtype*) &vp[4]; 		\
-	kvtype v5 = *(kvtype*) &vp[5];		\
-	kvtype v6 = *(kvtype*) &vp[6]; 		\
-	kvtype v7 = *(kvtype*) &vp[7];
+	vtype v0 = *(vtype*) &vp[0]; 		\
+	vtype v1 = *(vtype*) &vp[1]; 		\
+	vtype v2 = *(vtype*) &vp[2]; 		\
+	vtype v3 = *(vtype*) &vp[3]; 		\
+	vtype v4 = *(vtype*) &vp[4]; 		\
+	vtype v5 = *(vtype*) &vp[5];		\
+	vtype v6 = *(vtype*) &vp[6]; 		\
+	vtype v7 = *(vtype*) &vp[7];
 
 #define FINALIZE_NEXT_KEY_BIT_0g { 		\
-	kvtype m = mask01, va, vb, tmp; 	\
-	kvand(va, v0, m); 					\
-	kvand_shl1(vb, v1, m); 				\
-	kvand_shl_or(va, v2, m, 2); 		\
-	kvand_shl_or(vb, v3, m, 3); 		\
-	kvand_shl_or(va, v4, m, 4); 		\
-	kvand_shl_or(vb, v5, m, 5); 		\
-	kvand_shl_or(va, v6, m, 6); 		\
-	kvand_shl_or(vb, v7, m, 7); 		\
-	kvor(kp[0], va, vb);				\
+	vtype m = mask01, va, vb, tmp; 		\
+	vand(va, v0, m); 					\
+	vand_shl1(vb, v1, m); 				\
+	vand_shl_or(va, v2, m, 2); 			\
+	vand_shl_or(vb, v3, m, 3); 			\
+	vand_shl_or(va, v4, m, 4); 			\
+	vand_shl_or(vb, v5, m, 5); 			\
+	vand_shl_or(va, v6, m, 6); 			\
+	vand_shl_or(vb, v7, m, 7); 			\
+	vor(kp[0], va, vb);					\
 	kp += (gws * ITER_COUNT);			\
 }
 
 #define FINALIZE_NEXT_KEY_BIT_1g { 		\
-	kvtype m = mask02, va, vb, tmp; 	\
-	kvand_shr(va, v0, m, 1); 			\
-	kvand(vb, v1, m); 					\
-	kvand_shl1_or(va, v2, m); 			\
-	kvand_shl_or(vb, v3, m, 2); 		\
-	kvand_shl_or(va, v4, m, 3); 		\
-	kvand_shl_or(vb, v5, m, 4); 		\
-	kvand_shl_or(va, v6, m, 5); 		\
-	kvand_shl_or(vb, v7, m, 6); 		\
-	kvor(kp[0], va, vb); 				\
+	vtype m = mask02, va, vb, tmp; 		\
+	vand_shr(va, v0, m, 1); 			\
+	vand(vb, v1, m); 					\
+	vand_shl1_or(va, v2, m); 			\
+	vand_shl_or(vb, v3, m, 2); 			\
+	vand_shl_or(va, v4, m, 3); 			\
+	vand_shl_or(vb, v5, m, 4); 			\
+	vand_shl_or(va, v6, m, 5); 			\
+	vand_shl_or(vb, v7, m, 6); 			\
+	vor(kp[0], va, vb); 				\
 	kp += (gws * ITER_COUNT);			\
 }
 
 #define FINALIZE_NEXT_KEY_BIT_2g { 		\
-	kvtype m = mask04, va, vb, tmp; 	\
-	kvand_shr(va, v0, m, 2); 			\
-	kvand_shr(vb, v1, m, 1); 			\
-	kvand_or(va, v2, m); 				\
-	kvand_shl1_or(vb, v3, m); 			\
-	kvand_shl_or(va, v4, m, 2); 		\
-	kvand_shl_or(vb, v5, m, 3); 		\
-	kvand_shl_or(va, v6, m, 4); 		\
-	kvand_shl_or(vb, v7, m, 5); 		\
-	kvor(kp[0], va, vb); 				\
+	vtype m = mask04, va, vb, tmp; 		\
+	vand_shr(va, v0, m, 2); 			\
+	vand_shr(vb, v1, m, 1); 			\
+	vand_or(va, v2, m); 				\
+	vand_shl1_or(vb, v3, m); 			\
+	vand_shl_or(va, v4, m, 2); 			\
+	vand_shl_or(vb, v5, m, 3); 			\
+	vand_shl_or(va, v6, m, 4); 			\
+	vand_shl_or(vb, v7, m, 5); 			\
+	vor(kp[0], va, vb); 				\
 	kp += (gws * ITER_COUNT);			\
 }
 
 #define FINALIZE_NEXT_KEY_BIT_3g { 		\
-	kvtype m = mask08, va, vb, tmp; 	\
-	kvand_shr(va, v0, m, 3); 			\
-	kvand_shr(vb, v1, m, 2); 			\
-	kvand_shr_or(va, v2, m, 1); 		\
-	kvand_or(vb, v3, m); 				\
-	kvand_shl1_or(va, v4, m); 			\
-	kvand_shl_or(vb, v5, m, 2); 		\
-	kvand_shl_or(va, v6, m, 3); 		\
-	kvand_shl_or(vb, v7, m, 4); 		\
-	kvor(kp[0], va, vb); 				\
+	vtype m = mask08, va, vb, tmp; 		\
+	vand_shr(va, v0, m, 3); 			\
+	vand_shr(vb, v1, m, 2); 			\
+	vand_shr_or(va, v2, m, 1); 			\
+	vand_or(vb, v3, m); 				\
+	vand_shl1_or(va, v4, m); 			\
+	vand_shl_or(vb, v5, m, 2); 			\
+	vand_shl_or(va, v6, m, 3); 			\
+	vand_shl_or(vb, v7, m, 4); 			\
+	vor(kp[0], va, vb); 				\
 	kp += (gws * ITER_COUNT);			\
 }
 
 #define FINALIZE_NEXT_KEY_BIT_4g { 		\
-	kvtype m = mask10, va, vb, tmp; 	\
-	kvand_shr(va, v0, m, 4); 			\
-	kvand_shr(vb, v1, m, 3); 			\
-	kvand_shr_or(va, v2, m, 2); 		\
-	kvand_shr_or(vb, v3, m, 1); 		\
-	kvand_or(va, v4, m); 				\
-	kvand_shl1_or(vb, v5, m); 			\
-	kvand_shl_or(va, v6, m, 2); 		\
-	kvand_shl_or(vb, v7, m, 3); 		\
-	kvor(kp[0], va, vb); 				\
+	vtype m = mask10, va, vb, tmp; 		\
+	vand_shr(va, v0, m, 4); 			\
+	vand_shr(vb, v1, m, 3); 			\
+	vand_shr_or(va, v2, m, 2); 			\
+	vand_shr_or(vb, v3, m, 1); 			\
+	vand_or(va, v4, m); 				\
+	vand_shl1_or(vb, v5, m); 			\
+	vand_shl_or(va, v6, m, 2); 			\
+	vand_shl_or(vb, v7, m, 3); 			\
+	vor(kp[0], va, vb); 				\
 	kp += (gws * ITER_COUNT);			\
 }
 
 #define FINALIZE_NEXT_KEY_BIT_5g { 		\
-	kvtype m = mask20, va, vb, tmp; 	\
-	kvand_shr(va, v0, m, 5); 			\
-	kvand_shr(vb, v1, m, 4); 			\
-	kvand_shr_or(va, v2, m, 3); 		\
-	kvand_shr_or(vb, v3, m, 2); 		\
-	kvand_shr_or(va, v4, m, 1); 		\
-	kvand_or(vb, v5, m); 				\
-	kvand_shl1_or(va, v6, m); 			\
-	kvand_shl_or(vb, v7, m, 2); 		\
-	kvor(kp[0], va, vb); 				\
+	vtype m = mask20, va, vb, tmp; 		\
+	vand_shr(va, v0, m, 5); 			\
+	vand_shr(vb, v1, m, 4); 			\
+	vand_shr_or(va, v2, m, 3); 			\
+	vand_shr_or(vb, v3, m, 2); 			\
+	vand_shr_or(va, v4, m, 1); 			\
+	vand_or(vb, v5, m); 				\
+	vand_shl1_or(va, v6, m); 			\
+	vand_shl_or(vb, v7, m, 2); 			\
+	vor(kp[0], va, vb); 				\
 	kp += (gws * ITER_COUNT);			\
 }
 
 #define FINALIZE_NEXT_KEY_BIT_6g { 		\
-	kvtype m = mask40, va, vb, tmp; 	\
-	kvand_shr(va, v0, m, 6); 			\
-	kvand_shr(vb, v1, m, 5); 			\
-	kvand_shr_or(va, v2, m, 4); 		\
-	kvand_shr_or(vb, v3, m, 3); 		\
-	kvand_shr_or(va, v4, m, 2); 		\
-	kvand_shr_or(vb, v5, m, 1); 		\
-	kvand_or(va, v6, m); 				\
-	kvand_shl1_or(vb, v7, m); 			\
-	kvor(kp[0], va, vb); 				\
+	vtype m = mask40, va, vb, tmp; 		\
+	vand_shr(va, v0, m, 6); 			\
+	vand_shr(vb, v1, m, 5); 			\
+	vand_shr_or(va, v2, m, 4); 			\
+	vand_shr_or(vb, v3, m, 3); 			\
+	vand_shr_or(va, v4, m, 2); 			\
+	vand_shr_or(vb, v5, m, 1); 			\
+	vand_or(va, v6, m); 				\
+	vand_shl1_or(vb, v7, m); 			\
+	vor(kp[0], va, vb); 				\
 	kp += (gws * ITER_COUNT); 			\
 }
 
 #define FINALIZE_NEXT_KEY_BIT_7g { 		\
-	kvtype m = mask80, va, vb, tmp; 	\
-	kvand_shr(va, v0, m, 7); 			\
-	kvand_shr(vb, v1, m, 6); 			\
-	kvand_shr_or(va, v2, m, 5); 		\
-	kvand_shr_or(vb, v3, m, 4); 		\
-	kvand_shr_or(va, v4, m, 3); 		\
-	kvand_shr_or(vb, v5, m, 2); 		\
-	kvand_shr_or(va, v6, m, 1); 		\
-	kvand_or(vb, v7, m); 				\
-	kvor(kp[0], va, vb); 				\
+	vtype m = mask80, va, vb, tmp; 		\
+	vand_shr(va, v0, m, 7); 			\
+	vand_shr(vb, v1, m, 6); 			\
+	vand_shr_or(va, v2, m, 5); 			\
+	vand_shr_or(vb, v3, m, 4); 			\
+	vand_shr_or(va, v4, m, 3); 			\
+	vand_shr_or(vb, v5, m, 2); 			\
+	vand_shr_or(va, v6, m, 1); 			\
+	vand_or(vb, v7, m); 				\
+	vor(kp[0], va, vb); 				\
 	kp += (gws * ITER_COUNT);			\
 }
 
-__device__ void DES_bs_finalize_keys(
-	/*__global*/ bs_vector* des_bs_keys,
+__device__ void bitsplit_keys(
+	/*__global*/ bs_vector* bs_keys,
 	/*__global */ const keys_transfer* des_raw_keys)
 //#if USE_CONST_CACHED_INT_KEYS
 //				   constant
@@ -195,10 +188,11 @@ __device__ void DES_bs_finalize_keys(
 	const int section = blockIdx.x * blockDim.x + threadIdx.x; // get_global_id(0);
 	const int gws = gridDim.x * blockDim.x; // get_global_size(0);
 	
-	bs_vector *kp = &des_bs_keys[section];
+	bs_vector *kp = &bs_keys[section];
 
 	for (int ic = 0; ic < 8; ++ic) 
 	{
+		// TODO: this seems pretty inefficient CUDA memory access
 		const bs_vector *vp = &des_raw_keys[section].v[ic][0];
 		
 		LOAD_V
